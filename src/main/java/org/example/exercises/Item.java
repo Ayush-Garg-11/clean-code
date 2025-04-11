57ef0916cf514da1384fd56fa695719033d4c53d
Ayush Garg <ayush.garg@olx.in>
2025-04-12 00:01:31 +0530

    Clean Item

--- src/main/java/org/example/exercises/Item.java
+++ src/main/java/org/example/exercises/Item.java
@@ -2,22 +2,26 @@ package org.example.exercises;
 
 public class Item {
 
-	public String name;
+    public String name;
 
-	public int sellIn;
+    public int sellIn;
 
-	public int quality;
+    public int quality;
 
-	public Item(String name, int sellIn, int quality) {
-		this.name = name;
-		this.sellIn = sellIn;
-		this.quality = quality;
-	}
+    private static final String STRING_PREFIX = "name=";
+    private static final String STRING_BREAKS = ", ";
+    private static final String LINE_FEED = "\n";
 
-	@Override
-	public String toString() {
-		return "\n name=" + name + ", " + sellIn + ", " + quality + "\n" ;
-	}
-	
-	
+    public Item(String name, int sellIn, int quality) {
+        this.name = name;
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
+
+    @Override
+    public String toString() {
+        return LINE_FEED + STRING_PREFIX + name + STRING_BREAKS + sellIn + STRING_BREAKS + quality + LINE_FEED;
+    }
+
+
 }
